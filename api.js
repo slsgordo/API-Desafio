2d4339347a5dc666eba46078ff1875c3eaad6666
Silas <ssilas102@gmail.com>
2024-08-02 15:23:32 -0300

    Adicionado formato query e operação "dobro".

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const APi = express()
 
 APi.listen(5000, () => console.log('SUCESSO'))
 
-APi.get('/calculador/somar/:n1/:n2', (req, resp) => {
+APi.get('/calculadora/somar/:n1/:n2', (req, resp) => {
     let n1 = Number(req.params.n1)
     let n2 = Number(req.params.n2)
 
@@ -16,7 +16,7 @@ APi.get('/calculador/somar/:n1/:n2', (req, resp) => {
     })
 })
 
-APi.get('/calculador/subtrair/:n1/:n2', (req, resp) => {
+APi.get('/calculadora/subtrair/:n1/:n2', (req, resp) => {
     let n1 = Number(req.params.n1)
     let n2 = Number(req.params.n2)
 
@@ -29,7 +29,7 @@ APi.get('/calculador/subtrair/:n1/:n2', (req, resp) => {
     })
 })
 
-APi.get('/calculador/multiplicar/:n1/:n2', (req, resp) => {
+APi.get('/calculadora/multiplicar/:n1/:n2', (req, resp) => {
     let n1 = Number(req.params.n1)
     let n2 = Number(req.params.n2)
 
@@ -42,7 +42,7 @@ APi.get('/calculador/multiplicar/:n1/:n2', (req, resp) => {
     })
 })
 
-APi.get('/calculador/dividir/:n1/:n2', (req, resp) => {
+APi.get('/calculadora/dividir/:n1/:n2', (req, resp) => {
     let n1 = Number(req.params.n1)
     let n2 = Number(req.params.n2)
 
@@ -52,5 +52,79 @@ APi.get('/calculador/dividir/:n1/:n2', (req, resp) => {
         val1: n1,
         val2: n2,
         res: div
+    })
+}) 
+
+APi.get('/calculadora/dobro/:val', (req, resp) => {
+    let val = Number(req.params.val)
+
+    let dobro = val*2
+
+    resp.send({
+        val: val,
+        res: dobro
+    })
+}) 
+
+APi.get('/calculadora/somar', (req, resp) => {
+    let n1 = Number(req.query.n1)
+    let n2 = Number(req.query.n2)
+
+    let soma = n1 + n2
+
+    resp.send({
+        val1: n1,
+        val2: n2,
+        res: soma
+    })
+})
+
+APi.get('/calculadora/subtrair', (req, resp) => {
+    let n1 = Number(req.query.n1)
+    let n2 = Number(req.query.n2)
+
+    let sub = n1 - n2
+
+    resp.send({
+        val1: n1,
+        val2: n2,
+        res: sub
+    })
+})
+
+APi.get('/calculadora/multiplicar', (req, resp) => {
+    let n1 = Number(req.query.n1)
+    let n2 = Number(req.query.n2)
+
+    let mult = n1 * n2
+
+    resp.send({
+        val1: n1,
+        val2: n2,
+        res: mult
+    })
+})
+
+APi.get('/calculadora/dividir', (req, resp) => {
+    let n1 = Number(req.query.n1)
+    let n2 = Number(req.query.n2)
+
+    let div = n1 / n2
+
+    resp.send({
+        val1: n1,
+        val2: n2,
+        res: div
+    })
+}) 
+
+APi.get('/calculadora/dobro', (req, resp) => {
+    let val = Number(req.query.val)
+
+    let dobro = val*2
+
+    resp.send({
+        val: val,
+        res: dobro
     })
 }) 
