d88c86b7e39a3cb002cadb4e293813921c774a46
Silas <ssilas102@gmail.com>
2024-08-02 15:03:20 -0300

    Soma concluída.

--- api.js
+++ api.js
@@ -1,1 +1,14 @@
-console.log(`Hello Wolrd!`);
+import express from "express";
+const APi = express()
+
+APi.listen(5000,()=>console.log('SUCESSO'))
+
+APi.get('/somar/:n1/:n2', (req, resp) => {
+    let n1 = Number(req.params.n1)
+    let n2 = Number(req.params.n2)
+
+    let soma = n1 + n2
+    resp.send({
+        soma : soma
+    })
+})  
