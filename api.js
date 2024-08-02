2b39057562a64f10057952e6e34b43eac49007a5
Silas <ssilas102@gmail.com>
2024-08-02 15:07:00 -0300

    Subtração adicionada.

--- api.js
+++ api.js
@@ -3,12 +3,22 @@ const APi = express()
 
 APi.listen(5000,()=>console.log('SUCESSO'))
 
-APi.get('/somar/:n1/:n2', (req, resp) => {
+APi.get('/calculador/somar/:n1/:n2', (req, resp) => {
     let n1 = Number(req.params.n1)
     let n2 = Number(req.params.n2)
 
     let soma = n1 + n2
     resp.send({
-        soma : soma
+        res : soma
     })
 })  
+
+APi.get('/calculador/subtrair/:n1/:n2', (req, resp) => {
+    let n1 = Number(req.params.n1)
+    let n2 = Number(req.params.n2)
+
+    let sub = n1 - n2
+    resp.send({
+        res : sub
+    })
+}) 
